7d470e90d92ea4953f9f5169ae0353518d4f3b43
Luque <leoc.luque@gmail.com>
2022-11-22 20:37:08 -0300

    adjustes

--- src/main/kotlin/com/twa/Application.kt
+++ src/main/kotlin/com/twa/Application.kt
@@ -9,33 +9,33 @@ import org.slf4j.LoggerFactory
 import java.io.File
 
 fun main() {
-    val keyStoreFile = File("build/keystorektor.jks")
-    val keystore = generateCertificate(
-        file = keyStoreFile,
-        keyAlias = "MyKeyKtor",
-        keyPassword = "123456",
-        jksPassword = "123456"
-    )
-//    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-//        .start(wait = true)
+//    val keyStoreFile = File("build/keystorektor.jks")
+//    val keystore = generateCertificate(
+//        file = keyStoreFile,
+//        keyAlias = "MyKeyKtor",
+//        keyPassword = "123456",
+//        jksPassword = "123456"
+//    )
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 
-    val environment = applicationEngineEnvironment {
-        log = LoggerFactory.getLogger("ktor.application")
-        connector {
-            port = 8080
-        }
-        sslConnector(
-            keyStore = keystore,
-            keyAlias = "MyKeyKtor",
-            keyStorePassword = { "123456".toCharArray() },
-            privateKeyPassword = { "123456".toCharArray() }) {
-            port = 8443
-            keyStorePath = keyStoreFile
-        }
-        module(Application::module)
-    }
-
-    embeddedServer(Netty, environment).start(wait = true)
+//    val environment = applicationEngineEnvironment {
+//        log = LoggerFactory.getLogger("ktor.application")
+//        connector {
+//            port = 8080
+//        }
+//        sslConnector(
+//            keyStore = keystore,
+//            keyAlias = "MyKeyKtor",
+//            keyStorePassword = { "123456".toCharArray() },
+//            privateKeyPassword = { "123456".toCharArray() }) {
+//            port = 8443
+//            keyStorePath = keyStoreFile
+//        }
+//        module(Application::module)
+//    }
+//
+//    embeddedServer(Netty, environment).start(wait = true)
 }
 
 fun Application.module() {
--- src/main/kotlin/com/twa/plugins/HTTP.kt
+++ src/main/kotlin/com/twa/plugins/HTTP.kt
@@ -9,15 +9,15 @@ import io.ktor.server.response.*
 import io.ktor.server.request.*
 
 fun Application.configureHTTP() {
-    install(HttpsRedirect) {
-            // The port to redirect to. By default 443, the default HTTPS port.
-            sslPort = 443
-            // 301 Moved Permanently, or 302 Found redirect.
-            permanentRedirect = true
-        }
-    install(HSTS) {
-        includeSubDomains = true
-    }
+//    install(HttpsRedirect) {
+//            // The port to redirect to. By default 443, the default HTTPS port.
+//            sslPort = 443
+//            // 301 Moved Permanently, or 302 Found redirect.
+//            permanentRedirect = true
+//        }
+//    install(HSTS) {
+//        includeSubDomains = true
+//    }
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
