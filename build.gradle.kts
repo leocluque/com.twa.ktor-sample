1c568497ab5f6d8572e3568589b1dbf33f98d436
Luque <leoc.luque@gmail.com>
2022-11-22 20:42:32 -0300

    adjustes

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ repositories {
     mavenCentral()
 }
 
+tasks {
+    create("stage").dependsOn("installDist")
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
